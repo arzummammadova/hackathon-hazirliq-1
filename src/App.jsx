512cfaf12ad24af0976e2d08e7f257f0a726a618
arzummammadova <arzuam-af106@code.edu.az>
2025-06-23 11:23:12 +0400

    salam

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Courses from './pages/Courses';
+import Layout from './components/layout/Layout';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <Courses />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-    <div>
+       return <RouterProvider router={router} />;
       
-    </div>
-  )
 }
 
 export default App
